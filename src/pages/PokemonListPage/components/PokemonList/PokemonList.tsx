dbe1c5612b940e0ee562e5a51c654bc6e7491e3a
BrunoMorales <your_email_address@example.com>
2022-05-21 22:50:36 -0500

    Merged conflicts and added pagination feature

--- src/pages/PokemonListPage/components/PokemonList/PokemonList.tsx
+++ src/pages/PokemonListPage/components/PokemonList/PokemonList.tsx
@@ -9,7 +9,9 @@ import "./pokemonList.scss";
 const PokemonList = () => {
 	const [pokemonPageList, setPokemonPageList] = useState<string[]>([]);
 	const [pokemonPage, setPokemonPage] = useState<Pokemon[]>([]);
-	const [pokemonPageIndex, setPokemonPageIndex] = useState<number>(1);
+	const [pokemonPageIndex, setPokemonPageIndex] = useState<number>(0);
+	const [manualPage, setManualPage] = useState<number | string>("");
+	const [errorMessage, setErrorMessage] = useState<string>("");
 
 	useEffect(() => {
 		// Getting all pokemons urls first, already paginated
@@ -25,12 +27,68 @@ const PokemonList = () => {
 				setPokemonPage(pokemons);
 			}
 		);
-	}, [pokemonPageList]);
+	}, [pokemonPageList, pokemonPageIndex]);
+
+	const handleGoToPage = (e) => {
+		e.preventDefault();
+		const pageToGo = Number(manualPage) - 1;
+		if (
+			(pageToGo || pageToGo === 0) &&
+			pageToGo <= pokemonPageList.length &&
+			pageToGo >= 0
+		) {
+			setPokemonPageIndex(pageToGo);
+			setManualPage("");
+			setErrorMessage("");
+		} else {
+			pageToGo
+				? setErrorMessage("there's no such page")
+				: setErrorMessage("");
+		}
+	};
+
+	const handlePreviousPage = (e) => {
+		e.preventDefault();
+		pokemonPageIndex > 0 && setPokemonPageIndex(pokemonPageIndex - 1);
+	};
+
+	const handleNextPage = (e) => {
+		e.preventDefault();
+		pokemonPageIndex <= pokemonPageList.length &&
+			setPokemonPageIndex(pokemonPageIndex + 1);
+	};
 
 	return (
 		<section className='pokemon-list'>
-			<div>
-				page {pokemonPageIndex} of {pokemonPageList.length}
+			<div className='pagination-container'>
+				<div className='manual-pagination'>
+					<input
+						value={manualPage || ""}
+						onChange={(e) => setManualPage(Number(e.target.value))}
+						type='number'
+					/>
+					<button onClick={handleGoToPage}>go to page</button>
+					<p>{errorMessage}</p>
+				</div>
+				<div className='pagination-controls'>
+					<button
+						disabled={pokemonPageIndex <= 0}
+						onClick={handlePreviousPage}
+					>
+						Previous page
+					</button>
+					<p>
+						page {pokemonPageIndex + 1} of {pokemonPageList.length}
+					</p>
+					<button
+						disabled={
+							pokemonPageIndex >= pokemonPageList.length - 1
+						}
+						onClick={handleNextPage}
+					>
+						Next page
+					</button>
+				</div>
 			</div>
 			<div>
 				{pokemonPage?.map((pokemon) => (
