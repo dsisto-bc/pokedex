5b5731a680bd232979a459c707ab3c957008b21e
BrunoMorales <your_email_address@example.com>
2022-05-23 20:32:57 -0500

    styled detail page

--- src/pages/PokemonDetailPage/DetailSections/PokemonProfileSection/PokemonProfileSection.tsx
+++ src/pages/PokemonDetailPage/DetailSections/PokemonProfileSection/PokemonProfileSection.tsx
@@ -4,9 +4,9 @@ import { ProfileProps } from "./Types";
 import "./styles.scss";
 
 const PokemonDescriptionSection = ({ profile: { pokemonName, image, mainType } }: ProfileProps) => (
-	<section className={`profile type-color--${mainType}`}>
+	<section className={` type-color--${mainType} profile`}>
 		<h1 className='name'>{pokemonName}</h1>
-		<img className='picture' src={image} alt={`pokemon picture`} />
+		<img className={`picture type-color--${mainType}`} src={image} alt={`pokemon picture`} />
 	</section>
 );
 
